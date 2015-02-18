27008032bb61994726b134d9a46e9737efab46db
Vijay Sharma <vijay.sharm@gmail.com>
2015-02-18 16:08:56 -0500

    updated group id

--- sample/src/main/java/ca/vijaysharma/materialdesignicons/sample/MainActivity.java
+++ sample/src/main/java/ca/vijaysharma/materialdesignicons/sample/MainActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.vijaysharma.materialdesignicons.sample;
+package ca.vijaysharma.materialdesignicons.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
